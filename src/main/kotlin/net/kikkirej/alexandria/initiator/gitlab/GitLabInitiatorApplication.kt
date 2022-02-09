a786262697bdf411f4934e959ffbd8bc3038a689
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-02-09 14:36:47 +0100

    🐛

--- src/main/kotlin/net/kikkirej/alexandria/initiator/gitlab/GitLabInitiatorApplication.kt
+++ src/main/kotlin/net/kikkirej/alexandria/initiator/gitlab/GitLabInitiatorApplication.kt
@@ -2,8 +2,10 @@ package net.kikkirej.alexandria.initiator.gitlab
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class GitLabInitiatorApplication
 
 fun main(args: Array<String>) {
